ac3c737d77784f17c8c4c9835ce657c2b0c3706e
mj0323 <jackjemmy72@gmail.com>
2021-04-14 15:29:13 +0800

    Initial Commit

---
+++ iOS-Final/iOS_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_FinalApp.swift
+//  iOS-Final
+//
+//  Created by User05 on 2021/4/14.
+//
+
+import SwiftUI
+
+@main
+struct iOS_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
