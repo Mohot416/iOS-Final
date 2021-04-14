abcfb596b31b46a0daac978ebf98c17b391eca62
mj0323 <jackjemmy72@gmail.com>
2021-04-14 16:01:15 +0800

    安裝並設定了 FireBase套件

--- iOS-Final/iOS_FinalApp.swift
+++ iOS-Final/iOS_FinalApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct iOS_FinalApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
